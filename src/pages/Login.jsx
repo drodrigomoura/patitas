e39719868dfcdfde3b61a1a329e185fe54ad5fda
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-08-20 12:54:24 -0300

    Clase 02: React Router, inicio de sesión y registro de usuarios part 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { auth, provider } from '../utils/firebase';
 
-const Login = () => (
-  <div className="Login">
-    <div className="Login-container">
-      <div className="Login-content">
-        <h2>Crear cuenta o Iniciar Sesión</h2>
-        <button>
-            <i>FB</i>
-            <span>Iniciar sesión</span>
-        </button>
+const Login = (props) => {
+
+  const LoginFacebook = () => {
+    auth().signInWithPopup(provider)
+      .then(({ user }) => {
+        props.history.push('/panel')
+      });
+  }
+
+  const LogoutFacebook = () => {
+    auth().signOut()
+      .then(() => {
+        props.history.push('/')
+      })
+  }
+
+  return (
+    <div className="Login">
+      <div className="Login-container">
+        <div className="Login-content">
+          <h2>Crear cuenta o Iniciar Sesión</h2>
+          <button onClick={LoginFacebook}>
+              <i className="fab fa-facebook-square"/>
+              <span>Iniciar sesión</span>
+          </button>
+          <button onClick={LogoutFacebook}>
+            Cerrar Sesión
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+    )
+};
 
 export default Login;
